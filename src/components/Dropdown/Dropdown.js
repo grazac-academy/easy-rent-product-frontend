6d9924aecfb6520d13f4855f1bc6be228ab24fe1
Mayowa dabiri <mayowad43@gmail.com>
2022-04-27 22:36:39 +0100

    updated hero section

--- src/components/Dropdown/Dropdown.js
+++ src/components/Dropdown/Dropdown.js
@@ -1,27 +1,27 @@
 import React, { useState } from 'react';
 import Select from 'react-select';
+import './Dropdown.css';
 
+const Dropdown = ({ options, placeholder, src, title }) => {
+  const [selectedOption, setSelectedOption] = useState(null);
 
-const options = [
-    { value: 'Lagos', label: 'Lagos' },
-    { value: 'Abuja', label: 'Abuja' },
-    { value: 'Ogun', label: 'Ogun' },
-  ];
-
-export default function App() {
-    const [selectedOption, setSelectedOption] = useState(null);
-  
-    return (
-      <div className="App">
+  return (
+    <div className="select_location">
+      <img className={'select_icons'} src={src} alt="location" />
+      <div>
+        <label className="select_label">{title}</label>
         <Select
           defaultValue={selectedOption}
           onChange={setSelectedOption}
           options={options}
           isSearchable={true}
-          placeholder='State'
-        //   autoFocus={true}
-          borderColor='#ffff'
+          placeholder={placeholder}
+          classNamePrefix="dropdown-select"
+          className="dropdown-select-container"
         />
       </div>
-    );
-  }
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,79 +1,44 @@
-import Location from '../../assets/location.svg'
-import Property from '../../assets/property.svg'
-import Price from '../../assets/price.svg'
-import Bedroom from '../../assets/bedroom.svg'
-import {Locations} from '../../Constants/index.js'
-import classes from './search.module.css'
-import Dropdown from '../Dropdown/Dropdown'
+import Location from '../../assets/location.svg';
+import Property from '../../assets/property.svg';
+import Price from '../../assets/price.svg';
+import Bedroom from '../../assets/bedroom.svg';
+import classes from './search.module.css';
+import Dropdown from '../Dropdown/Dropdown';
 
-const Search = () =>{
-    return(
-        <div>
+import { locations, propertyTypes } from '../../constants/index';
+
+const Search = () => {
+  return (
+    <div>
+      <div>
+        <div className={classes.rent}>
+          <p>Rent</p>
+        </div>
+        <div className={classes.detail}>
+          <div className={classes.innerdetail}>
+            <div className={classes.inner_detail}>
+              <Dropdown
+                options={locations}
+                placeholder={'State'}
+                src={Location}
+                title="Location"
+              />
+              <Dropdown
+                options={propertyTypes}
+                placeholder={'Choose Type'}
+                title="Property Type"
+                src={Property}
+              />
+              <Dropdown src={Price} title="Price Range" />
+              <Dropdown src={Bedroom} title="Bedroom" />
+            </div>
             <div>
-              <div className={classes.rent}>
-                  <p>Rent</p>
-              </div>
-              <div className={classes.detail}>
-                  <div className={classes.innerdetail}>
-                      <div className={classes.inner_detail}>
-                            <div className={classes.location}>
-                                <label>
-                                    <img className={classes.icons}  src={Location} alt= "location" />
-                                    Location
-                                </label>
-                                <Dropdown />
-                                {/* <input
-                                className={classes.input}
-                                id='location'
-                                placeholder="State"
-                                /> */}
-                                <datalist id='locations'>
-                                    {Locations.map((item, index) => (
-                                    <option value={item} key={index}></option>
-                                    ))}
-                                </datalist>
-                            </div>
-                            <div className={classes.location}>
-                                <label>
-                                    <img className={classes.icons} src={Property} alt= "property" />
-                                    Property Type
-                                </label>
-                                <input 
-                                className={classes.input}
-                                placeholder="Choose type"
-                                />
-                                <datalist></datalist>
-                            </div>
-                            <div className={classes.location}>
-                                <label>
-                                    <img className={classes.icons} src={Price} alt= "Price" />
-                                    Price Range
-                                </label>
-                                <input 
-                                className={classes.input}
-                                placeholder="Choose range"
-                                />
-                            </div>
-                            <div>
-                                <label>
-                                    <img className={classes.icons} src={Bedroom} alt= "Bedroom" />
-                                    Bedroom
-                                </label>
-                                <input 
-                                className={classes.input}
-                                placeholder="Choose number"
-                                />
-                            </div>
-                        </div>
-                        <div>
-                            <input 
-                            placeholder="Advanced search"
-                            />
-                        </div>
-                  </div>
-              </div>
+              <input placeholder="Advanced search" />
+            </div>
           </div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 export default Search;
