36da9e6200094cbf4c5df18157e12aa752fa9966
Mayowa dabiri <mayowad43@gmail.com>
2022-05-13 07:04:02 +0100

    bug fixes on responsivenes

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,11 @@
 import './Modal.css';
 
 const Modal = ({ children }) => {
-  return <div className="modal">{children}</div>;
+  return (
+    <div className="modal">
+      <div className="modal-container">{children}</div>
+    </div>
+  );
 };
 
 export default Modal;
