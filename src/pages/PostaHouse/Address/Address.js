8c47469d2a9f17940f2075603b25efb71d910fcf
abiodunolayinka <97895608+abiodunolayinka@users.noreply.github.com>
2022-05-18 13:19:23 +0100

    Header correction (#17)
    * headerCorrection
    
    * propertyDeatailsIntro
    
    * propertyDetailsPage
    
    * headerCorrection
    
    * this is the property details in header
    
    Co-authored-by: Olayinka Abiodun <olayinkaabiodun75@gmail.com>
    Co-authored-by: Mayowa dabiri <mayowad43@gmail.com>

---
+++ src/pages/PostaHouse/Address/Address.js
@@ -0,0 +1,12 @@
+import React from "react";
+import auth from '../../../layout/PostaHouse/auth'
+
+const Address = () => {
+  return (
+    <auth>
+        <p>gyfgffddxcxfdxdezds</p>
+    </auth>
+  );
+};
+
+export default Address;
