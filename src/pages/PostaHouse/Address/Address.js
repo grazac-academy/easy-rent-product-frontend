338292c3eb296d518c7bcd09c1e82096f1d6774d
Bello aisha <belloaishaaderonke@gmail.com>
2022-05-26 17:11:50 +0100

    validation

--- src/pages/PostaHouse/Address/Address.js
+++ src/pages/PostaHouse/Address/Address.js
@@ -1,13 +1,27 @@
-import React from "react";
+// import React from "react";
 import classes from "./Address.module.css";
+import React, { useState } from 'react';
+// import Dropdown from "components/Dropdown/Dropdown";
+import { locations } from "constants/index";
+// import Select from "react-select";
+
+
 
 const Address = () => {
+  const [address, setAddress] = useState("");
+  console.log(address)
   return (
    
         <div className={classes.mainContainer}>
           <div className= {classes.address}>
             <h3>Address</h3>
-            <input type="text" id="address" placeholder="Enter full address of the house"/>
+            <input 
+            type="text" 
+            id="address"  
+            placeholder="Enter full address of the house" 
+            onChange={(e) => setAddress(e.target.value)}
+            value={address} 
+            />
             <p>Note: Ensure your adress is as detailed as possible</p>
           </div>
           <div className={classes.addressFlex}>
@@ -17,7 +31,27 @@ const Address = () => {
             </div>
             <div className={classes.addressFlex1}>
               <h3>State</h3>
-              <input type="text" id="postalCode" placeholder="Select State"/>
+              <select >
+                {locations.map((item) => (<option>
+                  {item.label}
+                </option>))}
+              </select>
+              {/* <Select 
+              defaultValue={selectedOption}
+              onChange={setSelectedOption}
+              options={locations}
+              isSearchable={true}
+              placeholder={ 'Select state' }
+              classNamePrefix="address-select"
+              className="address-select-container"
+              /> */}
+              {/* <Dropdown 
+              options={locations}
+              placeholder={'State'}
+              
+              // placeholder={States}
+              /> */}
+              {/* <input type="text" id="postalCode" placeholder="Select State"/> */}
             </div>
           </div>
         </div>
