a5e1255248c10e9cd096d2ec0548c03c7cf3b950
Adekunle <97894834+awotipe@users.noreply.github.com>
2022-05-27 14:22:29 -0700

    post_a_house_landing (#19)
    Co-authored-by: Mayowa dabiri <mayowad43@gmail.com>

--- src/App.js
+++ src/App.js
@@ -16,8 +16,7 @@ import DashboardHome from 'pages/Dashboard/DashboardHome/DashboardHome';
 import DashboardProperties from 'pages/Dashboard/DashboardProperties/DashboardProperties';
 import DashboardProfile from 'pages/Dashboard/DashboardProfile/DashboardProfile';
 import Bookmarks from 'pages/Bookmarks/Bookmarks';
-
-
+import House from './pages/House/House';
 
 function App() {
   const location = useLocation();
@@ -40,16 +39,15 @@ function App() {
     );
   } else if (location.pathname.includes('/dashboard')) {
     return (
-        <Routes>
-          <Route path="/dashboard" element={<DashboardLayout/>}>
-            <Route path="home" element={<DashboardHome />} />
-            <Route path="properties" element={<DashboardProperties />} />
-            <Route path="profile" element={<DashboardProfile />} />
-          </Route>
-        </Routes>
+      <Routes>
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route path="home" element={<DashboardHome />} />
+          <Route path="properties" element={<DashboardProperties />} />
+          <Route path="profile" element={<DashboardProfile />} />
+        </Route>
+      </Routes>
     );
-  };
-
+  }
 
   return (
     <>
@@ -58,6 +56,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/property" element={<Property />} />
         <Route path="/apartmentlist" element={<Apartmentlist />} />
+        <Route path="/House" element={<House />} />
         <Route path="/bookmarks" element={<Bookmarks />} />
       </Routes>
       <Footer />
