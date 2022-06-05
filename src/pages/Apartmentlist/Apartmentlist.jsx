14c81af7552a8d8af436d830ec6db1f058d9f2b0
Mayowa dabiri <mayowad43@gmail.com>
2022-06-05 21:38:36 +0100

    bug fixes

--- src/pages/Apartmentlist/Apartmentlist.jsx
+++ src/pages/Apartmentlist/Apartmentlist.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import styles from './Apartmentlist.module.css';
 import ApartmentCard from 'components/ApartmentCard/ApartmentCard';
+// import { useBookmarkState } from '../../context/context';
+import PropertyListing from 'components/PropertyListing/Propertylisting';
 import { useBookmarkState } from 'context/context';
 
 const Apartmentlist = () => {
