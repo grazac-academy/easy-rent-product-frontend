58667c005a6401e2e0ff771bed423b069cc31fc6
devdabiri <mayowa.dabiri@launchbridge.ng>
2022-06-09 16:12:16 +0100

    'baseCorrection'

--- src/base/baseUrl.jsx
+++ src/base/baseUrl.jsx
@@ -1,15 +1,11 @@
 import axios from 'axios';
 
-export const url =
-  process.env.NODE_ENV === 'development'
-    ? 'https://eazy-rent.herokuapp.com/api/v1'
-    : 'api.eazy-rent.com/api/v1';
+export const url = 'https://eazy-rent.herokuapp.com/api/v1';
+// export const url =
+//   process.env.NODE_ENV === 'development'
+//     ? 'https://eazy-rent.herokuapp.com/api/v1'
+//     : 'api.eazy-rent.com/api/v1';
 
 export const baseUrl = axios.create({
   baseURL: `${url}`,
-  // headers: {
-  //   'Content-Type': 'application/json',
-  // // },
-  // timeout: 100000,
-  // timeoutErrorMessage: 'Request timed out, pls try again later',
 });
