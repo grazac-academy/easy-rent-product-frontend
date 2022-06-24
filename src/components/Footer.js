c3650a0aa6e15e2bf714f327aa6e85b1f9f3fe7d
abiodunolayinka <97895608+abiodunolayinka@users.noreply.github.com>
2022-06-24 08:21:15 +0100

    House correction (#40)
    * houseCorrection
    
    * coorrection

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,26 +8,26 @@ import location from "../assets/location.svg";
 function Footer() {
   return (
     <div class="bg-dark m-auto ">
-      <div class="m-auto md:m-0">
-        <div className="text-white w-2/4 flex m-auto text-primary text-3xl text-center md:text-2xl py-5 justify-center items-center">
+      <div class="m-auto">
+        <div className="text-white w-2/4 flex m-auto text-primary  text-center text-2xl py-5 justify-center items-center">
           <h1>Get Latest Updates From Us</h1>
         </div>
-        <div className="text-white w-2/4 flex m-auto justify-center items-center">
+        <div className="text-white md:w-2/4 w-3/4 flex m-auto justify-center items-center">
           <form class="flex flex-row w-full rounded rounded-1-lg">
             <input
-              class="h-16 bg-white text-grey-darker rounded-l-lg text-grey-darkest  font-primary w-full py-1 px-2 outline-none text-lg text-black"
+              class="py-19 bg-white text-grey-darker rounded-l-lg text-grey-darkest  font-primary w-full py-1 px-2 outline-none text-lg text-black"
               type="text"
               placeholder="Email"
             />
-            <span class="flex items-center bg-white rounded rounded-l-none border-0 px-3 font-primary text-grey-100">
-              <button class="bg-gredient-dark  bg-button hover:bg-dark  text-lg text-white font-primary w py-3 px-6 rounded">
-                Subscibe
+            <span class="flex items-center bg-white rounded rounded-l-none border-0  font-primary p-2 text-grey-100">
+              <button class="bg-gredient-dark  bg-button hover:bg-dark text-lg p-2  md:text-lg text-white font-primary md:w-full md:py-3 md:px-6 rounded">
+                Subscribe
               </button>
             </span>
           </form>
         </div>
       </div>
-      <div class="md:flex  flex-cols sm:grid grid-cols-2 gap-10 md:justify-evenly justify-between m-auto md:ml-20 pt-10 md:pb-20 w-4/5  ">
+      <div class="md:flex  flex-cols gap-10 md:justify-evenly justify-between m-auto md:ml-20 pt-10 md:pb-20 w-4/5  ">
         <div class="md:w-1/6">
           <div className="header-logo md:flex flex-cols ">
             <img src={footerLogo} alt="logo" />
@@ -36,7 +36,7 @@ function Footer() {
             <div className=" text-gray-400">
               <img src={location} alt="logo" />
             </div>
-            <h3 className=" text-gray-400 word-break font-primary font-sans w-5/6 text-xl ">
+            <h3 className=" text-gray-400 word-break font-primary font-sans  text-base ">
               34, Grazac Road Idi-aba, Abeokuta, Ogun
             </h3>
           </div>
@@ -48,8 +48,8 @@ function Footer() {
         </div>
 
         <div>
-          <ul className="no-underline lg:py-4 text-gray-400 text-xl">
-            <li className="font-primary mb-4 text-white text-2xl">Discover</li>
+          <ul className="cursor-pointer no-underline lg:py-4 text-gray-400 text-base">
+            <li className="font-primary mb-4 text-white text-lg ">Discover</li>
             <li className="mb-4">lagos</li>
             <li className="mb-4">Ogun</li>
             <li className="mb-4">Abuja</li>
@@ -57,8 +57,8 @@ function Footer() {
         </div>
 
         <div>
-          <ul className=" font-primary text-gray-400 text-xl">
-            <li className="font-head mb-4 md:pt-4  text-white text-2xl">
+          <ul className="cursor-pointer  font-primary text-gray-400 text-base">
+            <li className="font-head mb-4 md:pt-4 text-lg text-white ">
               List of Categories
             </li>
             <li className="mb-4">Apartment</li>
@@ -70,8 +70,8 @@ function Footer() {
         </div>
 
         <div className="">
-          <ul className="no-underline py-4 text-gray-400 text-xl">
-            <li className="font-primary mb-4 text-white  text-2xl">
+          <ul className="cursor-pointer no-underline py-4 text-gray-400 text-base">
+            <li className="font-primary mb-4 text-white text-lg  ">
               Quick Link
             </li>
 
