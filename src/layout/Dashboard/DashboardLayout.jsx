f661ae6fd7021b7701d8c95e5e3025f8398498fd
Olayinka Abiodun <abiodunolayinka75@gmail.com>
2022-06-27 14:11:23 +0100

    mobile responsive dashboard

--- src/layout/Dashboard/DashboardLayout.jsx
+++ src/layout/Dashboard/DashboardLayout.jsx
@@ -1,23 +1,35 @@
-import React from 'react';
-import styles from './DashboardLayout.module.css';
-import { Outlet } from 'react-router-dom';
-import DashboardSidebar from 'pages/Dashboard/DashboardSidebar/DashboardSidebar';
-import DashboardHeader from 'pages/Dashboard/DashboardHeader/DashboardHeader'
-
+import React from "react";
+import styles from "./DashboardLayout.module.css";
+import { Outlet } from "react-router-dom";
+import DashboardSidebar from "pages/Dashboard/DashboardSidebar/DashboardSidebar";
+import DashboardSidebarMobile from "pages/Dashboard/DashboardSidebarMobile/DashboardSidebarMobile";
+import DashboardHeader from "pages/Dashboard/DashboardHeader/DashboardHeader";
+import { useState } from "react";
+import { FaTimes, FaBars } from "react-icons/fa";
 
 const DashboardLayout = () => {
-
-
-
-
+  const [toggle, setToggle] = useState(true);
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
 
   return (
     <div className={styles.db_container}>
-      <DashboardSidebar />
+      {toggle ? (
+        <button onClick={handleToggle} className={styles.hamburger}>
+          <FaBars />
+        </button>
+      ) : (
+        <button onClick={handleToggle} className={styles.hamburger}>
+          <FaTimes />
+        </button>
+      )}
+      {!toggle ? <DashboardSidebarMobile /> : <DashboardSidebar />}
+
       <div className={styles.db_content}>
-        <DashboardHeader/>
+        <DashboardHeader />
         <main className={styles.mainOutlet}>
-           <Outlet />
+          <Outlet />
         </main>
       </div>
     </div>
---
+++ src/pages/Dashboard/DashboardSidebarMobile/DashboardSidebarMobile.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import styles from './DashboardSidebarMobile.module.css';
+import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { HiViewGrid } from 'react-icons/hi';
+import { MdDashboard } from 'react-icons/md';
+import { FaUser } from 'react-icons/fa';
+import { MdHouse } from 'react-icons/md';
+import { RiLogoutBoxFill } from 'react-icons/ri';
+import Logo from 'assets/Dashboard/db_logo.png';
+import { useBookmarkState } from 'context/context';
+
+const DashboardSidebarMobile = () => {
+  const { isLoggedIn, setIsLoggedIn } = useBookmarkState();
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    sessionStorage.setItem('userToken', '');
+    sessionStorage.clear();
+    setIsLoggedIn(false);
+    navigate('/');
+  };
+
+  return (
+    <div className={styles.db_sidebarContainer}>
+      <div className={styles.db_sidebar}>
+        <Link to="/">
+        <div className={styles.db_sidebarLogo}>
+          <img src={Logo} alt="Logo" />
+          </div>
+        </Link>
+        <nav className={styles.nav}>
+          <NavLink
+            to={'/dashboard/overview'}
+            className={({ isActive }) =>
+              [styles.navItem, isActive ? styles.active : undefined].join(' ')
+            }
+          >
+            <div className={styles.icon}>
+              <MdDashboard />
+            </div>
+            <div className={styles.navItemTitle}>Overview</div>
+          </NavLink>
+          <NavLink
+            to={'/dashboard/properties'}
+            className={({ isActive }) =>
+              [styles.navItem, isActive ? styles.active : undefined].join(' ')
+            }
+          >
+            <div className={styles.icon}>
+              <MdHouse />
+            </div>
+            <div className={styles.navItemTitle}>Properties</div>
+          </NavLink>
+          <NavLink
+            to={'/dashboard/profile'}
+            className={({ isActive }) =>
+              [styles.navItem, isActive ? styles.active : undefined].join(' ')
+            }
+          >
+            <div className={styles.icon}>
+              <FaUser />
+            </div>
+            <div className={styles.navItemTitle}>Profile</div>
+          </NavLink>
+        </nav>
+
+        <button className={styles.logout} onClick={handleLogout}>
+          <div className={styles.logoutIcon}>
+            <RiLogoutBoxFill />
+          </div>
+          <div className={styles.logoutColor}>Logout</div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardSidebarMobile;
